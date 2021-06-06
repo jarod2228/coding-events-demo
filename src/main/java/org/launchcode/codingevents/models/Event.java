bef511f8e1c219da8417763bc4c56244e7299b8c
Jarod Waggener <jarod2228@rocketmail.com>
2021-06-05 19:22:03 -0500

    finished chapter 13 exercise, does not quite work. Need small tweaks from lecture Monday

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
@@ -7,16 +8,51 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Your name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Your description was too long. Must be 500 characters or less.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required.")
+    @Email
+    private String contactEmail;
+
+    @NotBlank(message = "Must include a location consisting of at least 2 but no more than 50 characters.")
+    @Size(min = 2, max = 50)
+    @NotNull(message = "Null values are not allowed.")
+    private String location;
+
+    @AssertTrue(message = "All events are registration-only.")
+    @NotBlank(message = "Please check a box.")
+    @NotNull(message = "Please check a box.")
+    private boolean mustRegister;
+
+    @Size(min = 1, message = "Event must have at least 1 expected attendee.")
+    @NotBlank(message = "Event must have an attendee estimation given in the form of a number above 0.")
+    @NotNull(message = "Null values are not allowed. Enter the number of expected attendees.")
+    private int attendees;
+
+    @NotBlank(message = "Please check a box.")
+    @NotNull(message = "Please check a box.")
+    private boolean isOutsideFoodAndDrinkAllowed;
+
+
+    public Event(String name, String description, String contactEmail, String location, boolean mustRegister, int attendees, boolean isOutsideFoodAndDrinkAllowed) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.mustRegister = mustRegister;
+        this.attendees = attendees;
+        this.isOutsideFoodAndDrinkAllowed = isOutsideFoodAndDrinkAllowed;
         this.id = nextId;
         nextId++;
 
     }
+
+    public Event() {}
 
     public String getName() {
         return name;
@@ -34,8 +70,44 @@ public class Event {
         this.description = description;
     }
 
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public boolean isMustRegister() {
+        return mustRegister;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
+    }
+
+    public boolean isOutsideFoodAndDrinkAllowed() {
+        return isOutsideFoodAndDrinkAllowed;
+    }
+
+    public void setOutsideFoodAndDrinkAllowed(boolean outsideFoodAndDrinkAllowed) {
+        isOutsideFoodAndDrinkAllowed = outsideFoodAndDrinkAllowed;
     }
 
     @Override
