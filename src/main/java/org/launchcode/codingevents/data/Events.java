7626651bea579753aa49be90c145e703f2d653e3
Jarod Waggener <jarod2228@rocketmail.com>
2021-06-21 14:05:10 -0500

    finished ch 17.2 video, added events class as text underneath instructed. it was not in the video...

---
+++ src/main/java/org/launchcode/codingevents/data/Events.java
@@ -0,0 +1,58 @@
+//package org.launchcode.codingevents.data;
+//
+//public class Events {
+//    private int id;
+//    private String name;
+//    private String description;
+//    private String contactEmail;
+//    private String type;
+//
+//    public Events(String name, String description, String contactEmail, String type) {
+//        this.name = name;
+//        this.description = description;
+//        this.contactEmail = contactEmail;
+//        this.type = type;
+//    }
+//
+//    public Events() {}
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getContactEmail() {
+//        return contactEmail;
+//    }
+//
+//    public void setContactEmail(String contactEmail) {
+//        this.contactEmail = contactEmail;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//}
